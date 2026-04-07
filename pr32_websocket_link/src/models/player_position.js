89ecb86ed3db6a552c59fbd9cd2c864dc79009e3
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2026-04-07 18:25:49 +0200

    Changing models to work with Sequelize and MongoDb

--- pr32_websocket_link/src/models/player_position.js
+++ pr32_websocket_link/src/models/player_position.js
@@ -1,6 +1,28 @@
-const PlayerPosition = {
-    x: 0,
-    y: 0,
-};
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const PlayerPosition = sequelize.define('PlayerPosition', {
+    playerPositionId: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    dxPosition: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+    dyPosition: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+});
 
 module.exports = PlayerPosition;
--- pr32_websocket_link/src/models/session.js
+++ pr32_websocket_link/src/models/session.js
@@ -1,7 +1,24 @@
-const Session = {
-    sessionId: null,
-    distanciaTotal: 0,
-    message: '',
-}
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+const Session = sequelize.define('Session', {
+    sessionId: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    totalDistance: {
+        type: DataTypes.FLOAT,
+        defaultValue: 0
+    }
+});
 
 module.exports = Session;
